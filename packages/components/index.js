4a9d0ba161096f799e3996ba1c09634384c3bbf2
Dylan0229 <xht13414851399@163.com>
2022-09-08 00:13:19 +0800

    feat: 新增组件

--- packages/components/index.js
+++
@@ -1,7 +0,0 @@
-import {count} from '@xht/hooks';
-
-console.log(count);
-
-const fn = ()=>{
-    console.log(33)
-}
---
+++ packages/components/rollup.config.js
@@ -0,0 +1,20 @@
+import pkJson from "./package.json";
+import swc from "rollup-plugin-swc";
+import swcConfig from "../../.swcrc.json";
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "./index.ts",
+  output: {
+    format: "esm",
+    file: pkJson.module,
+  },
+  plugins: [
+    swc({
+      ...swcConfig,
+    }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+  ],
+};
